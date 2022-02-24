b26342adb086cef1cc8b77aad1562285cd5debfe
olaalshaikhali <olaalshaikhali@gmail.com>
2022-02-24 02:17:21 +0200

    figma

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import react from 'react';
+
+//style
+import './App.css';
+
+//component
+import ChoosYourPlan from './component/ChooseYourPlan';
+import HugeGlobal from './component/HugeGlobal';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ChoosYourPlan />
+      <HugeGlobal />
+    </div>
+  );
+}
+
+export default App;
